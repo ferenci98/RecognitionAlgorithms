fbc3d72c4159a26bff99f1c548855214f27cede0
ferenci98 <ferenci.erik00@gmail.com>
2022-04-23 22:45:28 +0200

    added basic pages

---
+++ Client/Pages/RecognitionView.razor.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace Client.Pages
+{
+	public partial class RecognitionView
+	{
+        private List<IBrowserFile> loadedFiles = new();
+        private bool isLoading;
+
+        private void LoadFiles(InputFileChangeEventArgs e)
+        {
+            isLoading = true;
+            loadedFiles.Clear();
+
+            foreach (var file in e.GetMultipleFiles(5))
+            {
+                try
+                {
+                    loadedFiles.Add(file);
+                }
+                catch (Exception ex)
+                {
+               
+                }
+            }
+
+            isLoading = false;
+        }
+
+    }
+}
+
